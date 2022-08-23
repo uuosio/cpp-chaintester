3c0e4a17c913c2da549402e00346e7a42b04f372
learnforpractice <learnforpractice@gmail.com>
2022-08-23 15:26:35 +0800

    Fixes for debugging

--- tests/hello/src/hello.cpp
+++ tests/hello/src/hello.cpp
@@ -9,17 +9,3 @@ void hello::check( name nm ) {
    print_f("Name : %\n", nm);
    eosio::check(nm == "hello"_n, "check name not equal to `hello`");
 }
-
-#define EOSIO_DISPATCH_NATIVE( TYPE, MEMBERS ) \
-extern "C" { \
-   __attribute__ ((visibility ("default"))) void native_apply( uint64_t receiver, uint64_t code, uint64_t action ) { \
-      if( code == receiver ) { \
-         switch( action ) { \
-            EOSIO_DISPATCH_HELPER( TYPE, MEMBERS ) \
-         } \
-         /* does not allow destructor of thiscontract to run: eosio_exit(0); */ \
-      } \
-   } \
-}
-
-EOSIO_DISPATCH_NATIVE(hello, (hi)(check))
---
+++ tests/hello/src/main.cpp
@@ -0,0 +1,15 @@
+#include <hello.hpp>
+
+#define EOSIO_DISPATCH_NATIVE( TYPE, MEMBERS ) \
+extern "C" { \
+   __attribute__ ((visibility ("default"))) void native_apply( uint64_t receiver, uint64_t code, uint64_t action ) { \
+      if( code == receiver ) { \
+         switch( action ) { \
+            EOSIO_DISPATCH_HELPER( TYPE, MEMBERS ) \
+         } \
+         /* does not allow destructor of thiscontract to run: eosio_exit(0); */ \
+      } \
+   } \
+}
+
+EOSIO_DISPATCH_NATIVE(hello, (hi)(check))
