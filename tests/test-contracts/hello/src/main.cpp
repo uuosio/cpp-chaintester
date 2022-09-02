6b95921d81f30343632e1ee6b2d6cc7a51d39483
learnforpractice <learnforpractice@gmail.com>
2022-09-02 23:17:02 +0800

    Small improvements

--- tests/test-contracts/hello/src/main.cpp
+++ tests/test-contracts/hello/src/main.cpp
@@ -1,15 +1,14 @@
 #include <hello.hpp>
 
-#define EOSIO_DISPATCH_NATIVE( TYPE, MEMBERS ) \
-extern "C" { \
-   __attribute__ ((visibility ("default"))) void hello_native_apply( uint64_t receiver, uint64_t code, uint64_t action ) { \
-      if( code == receiver ) { \
-         switch( action ) { \
-            EOSIO_DISPATCH_HELPER( TYPE, MEMBERS ) \
-         } \
-         /* does not allow destructor of thiscontract to run: eosio_exit(0); */ \
-      } \
-   } \
+extern "C" __attribute__ ((visibility ("default"))) void hello_native_apply( uint64_t receiver, uint64_t code, uint64_t action ) {
+   if (code == receiver) {
+      switch(action) {
+         case "hi"_n.value:
+            eosio::execute_action( eosio::name(receiver), eosio::name(code), &hello::hi );
+            break;
+         case "check"_n.value:
+            eosio::execute_action( eosio::name(receiver), eosio::name(code), &hello::check );
+            break;
+      }
+   }
 }
-
-EOSIO_DISPATCH_NATIVE(hello, (hi)(check))
